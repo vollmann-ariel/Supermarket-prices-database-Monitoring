dc5210c73bd4ccd5fc778cd61c443c48d4089d53
Vollmann <vollmann.ariel@gmail.com>
2022-06-27 23:59:36 -0300

    Initial commit

--- main.py
+++ main.py
@@ -4,29 +4,30 @@ from time import sleep
 from random import random
 import json
 import pandas as pd
+import function.py
 
 prodTable = []
 
 for department in range(268, 274):
-    pageNumber = 1
+    pageNumber = 50
     while True:
         url = f'https://www.masonline.com.ar/{department}?map=productClusterIds&page={pageNumber}'
         soup = bs(requests.get(url).text)
-        prod = soup.find('template', attrs={'data-type': 'json', 'data-varname': '__STATE__'}).find('script').text
+        productsData = soup.find('template', attrs={'data-type': 'json', 'data-varname': '__STATE__'}).find('script').text
         pageNumber += 1
-        if not ('productId' in prod):
+        if not ('productId' in productsData):
             break
-        prod = json.loads(prod)
+        productsJsonData = json.loads(productsData)
 
         nextProd = ' '
-        for each in prod:
+        for each in productsData:
             if not ((nextProd in each) or ('Image' in each) or ('ROOT' in each)):
                 nextProd = each
-                singleProd = {'productId': prod[each]['productId'], 'productReference': prod[each]['productReference'],
-                              'productName': prod[each]['productName'], 'description': prod[each]['description'],
-                              'brand': prod[each]['brand'], 'link': prod[each]['link'],
-                              'highPrice': prod[(prod[each]['priceRange']['id'] + '.sellingPrice')]['highPrice'],
-                              'lowPrice': prod[(prod[each]['priceRange']['id'] + '.sellingPrice')]['lowPrice']}
+                singleProd = {'productId': productsJsonData[each]['productId'], 'productReference': productsJsonData[each]['productReference'],
+                              'productName': productsJsonData[each]['productName'], 'description': productsJsonData[each]['description'],
+                              'brand': productsJsonData[each]['brand'], 'link': productsJsonData[each]['link'],
+                              'highPrice': productsJsonData[(productsJsonData[each]['priceRange']['id'] + '.sellingPrice')]['highPrice'],
+                              'lowPrice': productsJsonData[(productsJsonData[each]['priceRange']['id'] + '.sellingPrice')]['lowPrice']}
                 prodTable.append(singleProd)
                 print(singleProd)
         delay = random()
@@ -34,3 +35,9 @@ for department in range(268, 274):
         sleep(delay)
 
 prodTableDF = pd.DataFrame(prodTable)
+
+prodTableDF.to_excel('excel file.xls', index=False)
+'''
+from google.colab import files
+files.download('excel file.xls')
+'''
